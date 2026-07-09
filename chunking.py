f0ce97872b4412583d3ee457513e6d8d1119566c
Y Sreevidya <yallasree03@gmail.com>
2026-07-09 21:16:53 +0530

    Refactor text extraction in chunking.py
    Refactored text extraction to use a single full_text variable instead of a list.

--- chunking.py
+++ chunking.py
@@ -4,15 +4,12 @@ pdf_path = r"C:\Users\cgkis\OneDrive\Documents\HEDIS-MY-2024-Measure-Description
 reader = PdfReader(pdf_path)
 
 
-# ---------- TEXT EXTRACTION ----------
-texts = []
+full_text = ""
 for page in reader.pages:
-    t = page.extract_text()
-    if t:
-        texts.append(t)
+    full_text += page.extract_text() + "\n"
 
-full_text = "\n".join(texts)
-print(full_text[:2000])
+print(full_text[:2000])  # preview
+
 
 import re
 
